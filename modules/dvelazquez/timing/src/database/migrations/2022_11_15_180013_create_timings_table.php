50cad751a4b543779b6bfdadd279958420e2d131
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-11-15 19:13:33 +0100

    - Created the timing migration

---
+++ modules/dvelazquez/timing/src/database/migrations/2022_11_15_180013_create_timings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timings', function (Blueprint $table) {
+            $table->id();
+            $table->integer("time");
+            $table->string("task");
+            $table->text("description");
+            $table->date("date");
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timings');
+    }
+}
