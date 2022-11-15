5fb351ff71ef2f309292698d0292692a3b03f79f
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-11-15 19:14:09 +0100

    - Created the timing model

---
+++ modules/dvelazquez/timing/src/app/Models/Timing.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DVelazquez\Timing\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Timing extends Model
+{
+    use HasFactory;
+}
